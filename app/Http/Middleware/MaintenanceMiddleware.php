bcf5cc491f9ef97a3c78b3dc4735b2e18bee005d
Kirk John L. Sieras <bebekirk123@gmail.com>
2025-04-02 07:24:09 +0800

    error folder for maintenance mode done

--- app/Http/Middleware/MaintenanceMiddleware.php
+++ app/Http/Middleware/MaintenanceMiddleware.php
@@ -1,17 +1,16 @@
 <?php
-
 namespace App\Http\Middleware;
 
+use App\Models\MaintenanceSetting;
 use Closure;
 use Illuminate\Http\Request;
-use App\Models\MaintenanceSetting;
 
 class MaintenanceMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
         $maintenance = MaintenanceSetting::where('key', 'maintenance_mode')->first();
-        $message = MaintenanceSetting::where('key', 'maintenance_message')->value('value');
+        $message     = MaintenanceSetting::where('key', 'maintenance_message')->value('value');
 
         if ($maintenance && $maintenance->value === 'true') {
             return response()->view('maintenance', compact('message'));
